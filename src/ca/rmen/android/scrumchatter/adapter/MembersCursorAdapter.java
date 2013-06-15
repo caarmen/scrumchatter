c1dc22fe3eebcc301614d148c85d37b982ed93f4
Carmen <c@rmen.ca>
2013-06-15 21:40:48 +0200

    Added layout for member list item

--- src/ca/rmen/android/scrumchatter/adapter/MembersCursorAdapter.java
+++ src/ca/rmen/android/scrumchatter/adapter/MembersCursorAdapter.java
@@ -3,9 +3,11 @@ package ca.rmen.android.scrumchatter.adapter;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import ca.rmen.android.scrumchatter.R;
 import ca.rmen.android.scrumchatter.provider.MemberCursorWrapper;
 
 public class MembersCursorAdapter extends CursorAdapter {
@@ -19,18 +21,24 @@ public class MembersCursorAdapter extends CursorAdapter {
 
 	@Override
 	public void bindView(View view, Context context, Cursor cursor) {
-		MemberCursorWrapper memberCursorWrapper = new MemberCursorWrapper(
-				cursor);
-		((TextView) view).setText(memberCursorWrapper.getName());
+		fillView(view, cursor);
 	}
 
 	@Override
 	public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+		LayoutInflater layoutInflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View view = layoutInflater.inflate(R.layout.member_list_item, null);
+		fillView(view, cursor);
+		return view;
+	}
+
+	private void fillView(View view, Cursor cursor) {
 		MemberCursorWrapper memberCursorWrapper = new MemberCursorWrapper(
 				cursor);
-		TextView view = new TextView(context);
-		((TextView) view).setText(memberCursorWrapper.getName());
-		return view;
+		TextView tvName = (TextView) view.findViewById(R.id.tv_name);
+		tvName.setText(memberCursorWrapper.getName());
+
 	}
 
 }
