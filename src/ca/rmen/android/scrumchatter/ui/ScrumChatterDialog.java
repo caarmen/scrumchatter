34cbd13e386a2d4fc350ccc9c3576b4241d8679a
Carmen Alvarez <c@rmen.ca>
2013-06-30 22:31:54 +0200

    Document ScrumChatterDialog.  Customize the selector for the list in the alert dialog

--- src/ca/rmen/android/scrumchatter/ui/ScrumChatterDialog.java
+++ src/ca/rmen/android/scrumchatter/ui/ScrumChatterDialog.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright 2013 Carmen Alvarez
+ *
+ * This file is part of Scrum Chatter.
+ *
+ * Scrum Chatter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Scrum Chatter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scrum Chatter. If not, see <http://www.gnu.org/licenses/>.
+ */
 package ca.rmen.android.scrumchatter.ui;
 
 import android.annotation.TargetApi;
@@ -11,14 +29,23 @@ import android.os.Build;
 import android.view.ContextThemeWrapper;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import ca.rmen.android.scrumchatter.R;
 
+/**
+ * Boy is customizing alert dialogs a pain in the booty. Tried the android-styled-dialogs library but it didn't fit the needs of this app: no support for alert
+ * dialogs with EditTexts, and not a clean way to manage clicks on the dialog buttons. Started out trying to copy the resources used for dialogs, one-by-one,
+ * from the core android framework, but that was more pain than the approach I decided to take in this class.
+ * 
+ * Only 3.x+ devices will have customized dialogs.
+ */
 public class ScrumChatterDialog {
 
     public static AlertDialog showChoiceDialog(Context context, int titleId, int choicesArrayId, DialogInterface.OnClickListener itemListener) {
         return showDialog(context, context.getString(titleId), null, null, context.getResources().getStringArray(choicesArrayId), itemListener);
 
     }
+
     public static AlertDialog showDialog(Context context, String title, String message, DialogInterface.OnClickListener positiveListener) {
         return showDialog(context, title, message, null, null, positiveListener);
 
@@ -50,6 +77,8 @@ public class ScrumChatterDialog {
         dialog.show();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
             int color = context.getResources().getColor(R.color.scrum_chatter_holo_purple);
+            ListView listView = dialog.getListView();
+            if (listView != null) listView.setSelector(R.drawable.selector);
             uglyHackReplaceButtonBackground(dialog, android.R.id.button1);
             uglyHackReplaceButtonBackground(dialog, android.R.id.button2);
             uglyHackReplaceButtonBackground(dialog, android.R.id.button3);
