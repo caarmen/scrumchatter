5fe5076914c4e157cd7dbc1d885909b344909a50
Carmen Alvarez <c@rmen.ca>
2013-07-20 11:07:37 +0200

    Issue #10 - add themed radio buttons, and dismiss dialog when selecting team

--- src/ca/rmen/android/scrumchatter/ui/Teams.java
+++ src/ca/rmen/android/scrumchatter/ui/Teams.java
@@ -102,6 +102,7 @@ public class Teams {
                     OnClickListener itemListener = new OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
                             // The user clicked on the "new team" item.
                             if (which == mTeamNames.length - 1) {
                                 createTeam();
