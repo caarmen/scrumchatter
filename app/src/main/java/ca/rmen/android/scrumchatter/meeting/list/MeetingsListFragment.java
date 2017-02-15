6eaaac5e94ecbbde2dade57c372a036aea585816
Carmen Alvarez <c@rmen.ca>
2017-02-15 23:02:30 +0100

    Android studio code inspection cleanup

--- app/src/main/java/ca/rmen/android/scrumchatter/meeting/list/MeetingsListFragment.java
+++ app/src/main/java/ca/rmen/android/scrumchatter/meeting/list/MeetingsListFragment.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013-2016 Carmen Alvarez
+ * Copyright 2013-2017 Carmen Alvarez
  * <p/>
  * This file is part of Scrum Chatter.
  * <p/>
@@ -33,9 +33,6 @@ import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
